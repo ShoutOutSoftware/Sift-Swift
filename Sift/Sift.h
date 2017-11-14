1d12da632c20c58ef223ff393133b072cc493349
Obaid Ahmed Mohammed <obaid@shoutoutsoftware.com>
2017-11-14 09:02:53 +0530

    Initial Commit

---
+++ Sift/Sift.h
@@ -0,0 +1,19 @@
+//
+//  Sift.h
+//  Sift
+//
+//  Created by Mohammed Obaid Ahmed on 14/11/17.
+//  Copyright © 2017 Shoutout Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Sift.
+FOUNDATION_EXPORT double SiftVersionNumber;
+
+//! Project version string for Sift.
+FOUNDATION_EXPORT const unsigned char SiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sift/PublicHeader.h>
+
+
