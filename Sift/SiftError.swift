57b1a443ca9b8618b884e737dbc67fdace9a2cc1
Obaid Ahmed Mohammed <obaid@shoutoutsoftware.com>
2019-06-12 02:13:19 +0530

    Updated sift error with work around for localized description dynamic dispatch. Updated version

--- Sift/SiftError.swift
+++ Sift/SiftError.swift
@@ -11,17 +11,16 @@ import Foundation
 public class SiftError: Error {
 
     public let message: String
-
+    
+    //We have to override both localized and error description to work around swift dispath rules
+    //Check out https://forums.swift.org/t/confusing-error-localizeddescription-output/5337/3
+    public var localizedDescription: String { return message }
+    public var errorDescription: String? {return localizedDescription}
+    
     init(message: String) {
         self.message = message
     }
-
+    
 }
 
-extension SiftError: LocalizedError {
-
-    public var errorDescription: String? {
-        return NSLocalizedString(message, comment: "SiftError")
-    }
-
-}
+extension SiftError: LocalizedError { }
