13226bd952ca6e8c2a96c8daec725fb2c3e10d24
Obaid Ahmed Mohammed <obaid@shoutoutsoftware.com>
2020-03-25 12:09:55 +0530

    Fixed typo in comment

--- Sift/SiftError.swift
+++ Sift/SiftError.swift
@@ -12,7 +12,7 @@ public class SiftError: Error {
 
     public let message: String
     
-    //We have to override both localized and error description to work around swift dispath rules
+    //We have to override both localized and error description to work around swift dispatch rules
     //Check out https://forums.swift.org/t/confusing-error-localizeddescription-output/5337/3
     public var localizedDescription: String { return message }
     public var errorDescription: String? {return localizedDescription}
